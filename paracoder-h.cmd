d5b8acc52f35763bdabde7a8ca2bdf095d903eeb
rayden <rayden@tut.by>
2024-02-16 04:13:06 +0300

    Upgraded SpringBoot to 3.2.2. Fixed byte-buddy-agent warning on tests. Configured Gradle task to support UTF-8 encoding. Changed application version to 1.0.3.

--- paracoder-h.cmd
+++ paracoder-h.cmd
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001 > nul
 
-java --enable-preview -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar .\build\libs\ParaCoder-1.0.2.jar --help
+java --enable-preview -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar .\build\libs\ParaCoder-1.0.3.jar --help
 
 pause
--- paracoder.cmd
+++ paracoder.cmd
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 > nul
 
-java --enable-preview -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar d:\java\prj\paracoder\build\libs\ParaCoder-1.0.2.jar --recurse --thread-count=5 -d %*
+java --enable-preview -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar d:\java\prj\paracoder\build\libs\ParaCoder-1.0.3.jar --recurse --thread-count=5 -d %*
 
 echo ERRORLEVEL:%ERRORLEVEL%
 
