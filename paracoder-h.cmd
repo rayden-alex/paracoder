72c3f7953cdc32eaa83e0d440d31870d1b0d50fc
rayden <rayden@tut.by>
2025-03-20 10:21:04 +0300

    Added generation and use an AOT-cache

--- paracoder-h.cmd
+++ paracoder-h.cmd
@@ -1,6 +1,8 @@
 @echo off
 chcp 65001 > nul
 
-java --enable-native-access=ALL-UNNAMED -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar .\build\libs\ParaCoder.jar --help
+@echo Started: %time%
+java -XX:AOTCache=paracoder.aot --enable-native-access=ALL-UNNAMED -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar .\build\libs\ParaCoder.jar --help
+@echo Completed: %time%
 
 pause
