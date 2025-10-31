d99481abd80772fb324c5a141acf60231eeb711a
rayden <rayden@tut.by>
2025-10-31 18:54:42 +0300

    Enable AOT cache and CompactObjectHeaders for better performance.

--- paracoder-aot-create-cache.cmd
+++ paracoder-aot-create-cache.cmd
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 > nul
 
-java -XX:AOTMode=create -XX:AOTConfiguration=paracoder.aotconf -XX:AOTCache=paracoder.aot --enable-native-access=ALL-UNNAMED -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar d:\java\prj\paracoder\build\libs\ParaCoder.jar
+java -XX:AOTCacheOutput=paracoder.aot -XX:+UseCompactObjectHeaders -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar d:\java\prj\paracoder\build\libs\ParaCoder.jar
 
 echo ERRORLEVEL:%ERRORLEVEL%
 
--- paracoder-h.cmd
+++ paracoder-h.cmd
@@ -2,7 +2,7 @@
 chcp 65001 > nul
 
 @echo Started: %time%
-java -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar .\build\libs\ParaCoder.jar --help
+java -XX:AOTCache=paracoder.aot -XX:+UseCompactObjectHeaders -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar .\build\libs\ParaCoder.jar --help
 @echo Completed: %time%
 
 pause
