04362a3e17ed6990545b9a3fac10b2776ecae3a5
rayden <rayden@tut.by>
2025-11-03 08:24:19 +0300

    First implementation of a customizable location of the config file.

--- src/main/java/by/rayden/paracoder/service/RecodeCommand.java
+++ src/main/java/by/rayden/paracoder/service/RecodeCommand.java
@@ -1,22 +1,36 @@
 package by.rayden.paracoder.service;
 
 import by.rayden.paracoder.config.PatternProperties;
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.io.FilenameUtils;
 import org.jetbrains.annotations.VisibleForTesting;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.boot.context.properties.bind.PropertySourcesPlaceholdersResolver;
+import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
+import org.springframework.boot.env.PropertySourceLoader;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.PropertiesPropertySource;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.PropertySourcesPropertyResolver;
+import org.springframework.core.io.PathResource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.nio.file.Path;
 import java.text.DecimalFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class RecodeCommand {
     /**
      * Map to replace invalid characters in file name to HomoGlyphs
@@ -37,6 +51,70 @@ public class RecodeCommand {
 
     private static final DateTimeFormatter FFMPEG_TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
     private final PatternProperties patternProperties;
+
+    public RecodeCommand() {
+        BindResult<PatternProperties> bindResult = bindObjectByYaml("pattern", PatternProperties.class,
+            "paracoder_commands.yml");
+
+        this.patternProperties = bindResult
+            .orElseThrow(() -> new IllegalArgumentException("No Paracoder commands found"));
+    }
+
+    private BindResult<PatternProperties> bindObjectByYaml(String prefix, Class<PatternProperties> target,
+                                                           String yamlPath) {
+        var factory = new YamlPropertiesFactoryBean();
+        factory.setResources(new PathResource(yamlPath));
+        Properties properties = Objects.requireNonNull(factory.getObject());
+
+        return getBinder(properties).bind(prefix, target);
+    }
+
+    /**
+     * @see org.springframework.boot.context.properties.ConfigurationPropertiesBinder#getBinder()
+     * @see YamlPropertiesFactoryBean#createProperties()
+     */
+    @SuppressWarnings("JavadocReference")
+    private Binder getBinder(Properties properties) {
+        // A property source which will be used to assign JavaBean properties.
+        var propertySource = new MapConfigurationPropertySource(properties);
+
+        // A property source which will be used to resolve placeholders.
+        // In this case, it is based on the same properties, since we do not want to use any other system properties,
+        // properties from the environment and from other configuration files.
+        var resolverPropertySource = new PropertiesPropertySource("mySource", properties);
+        var placeholdersResolver = new PropertySourcesPlaceholdersResolver(List.of(resolverPropertySource));
+
+        return new Binder(List.of(propertySource), placeholdersResolver);
+    }
+
+    public void RecodeCommand__() throws IOException {
+//        Yaml yaml = new Yaml(constructor, representer);
+//
+//
+//        Pattern properties = null;
+//        String content = null;
+//        try {
+//             content = Files.readString(Path.of("paracoder_commands.yml"), StandardCharsets.UTF_8);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        properties = yaml.load(content);
+//        this.patternProperties = properties.getPattern();
+//
+
+        PropertySourceLoader ypsl = new YamlPropertySourceLoader();
+        PropertySource<?> ps = ypsl.load("paracoder_commands.yml", new PathResource("paracoder_commands.yml"))
+                                   .getFirst();
+        MutablePropertySources propertySources = new MutablePropertySources();
+        propertySources.addLast(ps);
+
+        PropertySourcesPropertyResolver propertyResolver = new PropertySourcesPropertyResolver(propertySources);
+//        Pattern pattern = propertyResolver.getProperty("pattern", Pattern.class);
+        // propertyResolver.getProperty("pattern.commandTemplate.flac")
+
+//        this.patternProperties = Objects.requireNonNull(pattern).getPattern();
+
+    }
 
     public String getCommand(Path filePath) {
         String extension = FilenameUtils.getExtension(filePath.toString());
