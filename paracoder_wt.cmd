5630d01e79d065282806bc6e427c2e40db60933d
rayden <rayden@tut.by>
2025-11-07 02:38:26 +0300

    Added new CMD scripts and new YAML configurations for recoding images to JPEG and audio files to MP3

--- paracoder_wt.cmd
+++ paracoder_wt.cmd
@@ -1,1 +1,2 @@
-wt -d d:\java\prj\paracoder\ cmd /k "d:\java\prj\paracoder\paracoder.cmd" %*
+:: Use Windows Terminal
+wt -d d:\java\prj\paracoder\ cmd /k "d:\java\prj\paracoder\paracoder_OPUS.cmd" %*
