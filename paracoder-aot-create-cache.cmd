260ad9774ba0584ca1069d26fbf6c30dfe7441e3
rayden <rayden@tut.by>
2025-12-24 04:32:12 +0300

    Added usage a Spring AOT on building AOT Cache

--- paracoder-aot-create-cache.cmd
+++ paracoder-aot-create-cache.cmd
@@ -1,10 +1,8 @@
 @echo off
 chcp 65001 > nul
 
-java -XX:AOTCacheOutput=paracoder.aot -XX:+UseCompactObjectHeaders -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar d:\java\prj\paracoder\build\libs\ParaCoder.jar
+java -Dspring.aot.enabled=true -Dspring.context.exit=onRefresh -XX:AOTCacheOutput=paracoder.aot -XX:+UseCompactObjectHeaders -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar d:\java\prj\paracoder\build\libs\ParaCoder.jar
 
 echo ERRORLEVEL:%ERRORLEVEL%
-
-rem popd
 
 pause
