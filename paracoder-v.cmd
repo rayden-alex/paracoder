3b40debd970b3fb236038e4a91baf9675d39bedb
rayden <rayden@tut.by>
2026-01-11 15:02:32 +0300

    Enable spring.aot for "help" and "version" cmd.

--- paracoder-v.cmd
+++ paracoder-v.cmd
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001 > nul
 
-java -XX:AOTCache=paracoder.aot -XX:+UseCompactObjectHeaders -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar .\build\libs\ParaCoder.jar --version
+java -XX:AOTCache=paracoder.aot -XX:+UseCompactObjectHeaders -Dspring.aot.enabled=true -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -jar .\build\libs\extracted\ParaCoder.jar --version
 
 pause
